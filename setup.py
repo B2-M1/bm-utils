93d30d7906967e5ac957a39f41dbdda86b224bff
Millhouse, Benjamin <benjamin.millhouse@mines.sdsmt.edu>
2019-11-01 17:46:08 -0600

    Adding setup and main funciton

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='bm_utils',
+    version= '0.0.0',
+    url = 'https://github.com/B2-M1/bm-utils.git',
+    license= 'Apache Licence 2.0',
+    description= 'Some function in python that are useful'
+)
