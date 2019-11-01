92b866b70ee77a74b0483bc030eafbdb4405a7b6
Millhouse, Benjamin <benjamin.millhouse@mines.sdsmt.edu>
2019-11-01 17:56:08 -0600

    Updated to version 0.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='bm_utils',
-    version= '0.0.0',
+    version= '0.0.1',
     url = 'https://github.com/B2-M1/bm-utils.git',
     license= 'Apache Licence 2.0',
     description= 'Some function in python that are useful'
